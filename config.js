8306a29e79ab7e8df9d44241c466ea5eea05d547
kelseylogan <117530233+kelseylogan@users.noreply.github.com>
2023-05-13 18:26:36 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "paste_your_api_url_here"
+    API_URL: "https://fce5c742-143f-4d42-bfeb-b067aa610385.serverhub.practicum-services.com"
 }
