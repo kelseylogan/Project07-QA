6daf5e215cd88f4c1c3e3045ad167e0d0406803e
Kelsey Logan <kelseylogan619@gmail.com>
2023-05-13 18:57:21 -0400

    update postHandlers

--- tests/postHandlers.test.js
+++ tests/postHandlers.test.js
@@ -2,19 +2,41 @@
 const config = require('../config');
 
 const requestBody = {
-    // put your body here
+	"name": "Kelsey",
+	"cardId": 5
 }
 
-test('', async () => {
-    try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
-			method: 'POST',
-			headers: {
-			'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(requestBody)
-		});
-	} catch (error) {
-		console.error(error);
-	}
+test('Should return 201 status code', async () => {
+let actualStatusCode;
+try {
+	const response = await fetch(`${config.API_URL}/api/v1/kits`, {
+		method: 'POST',
+		headers: {
+		'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(requestBody)
+	});
+	actualStatusCode = response.status;
+} catch (error) {
+	console.error(error);
+}
+expect(actualStatusCode).toBe(201);
+});
+
+
+test('Response body should contain the name as Kelsey', async () => {
+let actualResponseBody;
+try {
+	const response = await fetch(`${config.API_URL}/api/v1/kits`, {
+		method: 'POST',
+		headers: {
+		'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(requestBody)
+	});
+	actualResponseBody = await response.json();
+} catch (error) {
+	console.error(error);
+}
+expect(actualResponseBody.name).toBe("Kelsey");
 });
